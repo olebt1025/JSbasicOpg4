8c168f396e6fe8bbd15dc74df1b5b28863f5c6cb
olebt1025 <olebt1025@gmail.com>
2025-12-04 13:16:51 +0100

    mainBranchShit

--- main.js
+++ main.js
@@ -5,10 +5,33 @@
 
 document.getElementById("diceRoll").addEventListener("click", rollDice);
 
+
+// this determines the vlue of the diceroll and sides is decided by the type of die selected in the selector.
 function rollDice(){
+
+    //parseInt converts the number in the string to a number
     const sides = parseInt(document.getElementById("dieType").value);
 
     const roll = Math.floor(Math.random() * sides) + 1;
 
-    document.getElementById("result").textContent = `you rolled a ${roll} on a d${sides}`;
+
+    //this puts the textcontent into the <p> element in the HTML
+    document.getElementById("result").textContent = `You rolled a ${roll} on a d${sides}`;
+
+    addToHistory(roll, sides);
 } 
+
+function addToHistory(roll, sides) {
+
+    //takes the list element from html and 
+    const list = document.getElementById("rollHistory");
+
+    //creates a new list item for every roll
+    const item = document.createElement("li");
+
+    item.textContent = ` You have rolled d${sides}: ${roll}`;
+
+
+    //take the newly created list item and puts it in the list
+    list.appendChild(item);
+}
